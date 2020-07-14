eaa9e82007ef53292d669d7e66a08c209417fb9b
Fahad Tariq <47610084+FahadTComsats@users.noreply.github.com>
2020-07-14 20:55:59 +0500

    Final

--- FahadCard/ContentView.swift
+++ FahadCard/ContentView.swift
@@ -10,12 +10,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.0)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                 
+                    Image("Fahad Tariq")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 180, height: 180)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.white,lineWidth: 5)
+                        )
+                    
+                    Text("Fahad Tariq")
+                        .font(Font.custom("Pacifico-Regular", size: 40))
+                        .bold()
+                        .foregroundColor(.white)
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                    Divider()
+                InfoView()
+                
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone Xs Max"))
     }
+}
+
+struct InfoView: View {
+    
+    
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 30)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(
+                HStack {
+                    Image(systemName: "phone.fill")
+                        .foregroundColor(.green)
+                    Text("+923214054291").bold()
+                    
+            })
+            .padding(.all)
+        
+    }
+    
 }
